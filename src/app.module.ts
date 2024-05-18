a1975b7f155e08723d5abb2d267d2205a217d5c9
Luciano Vianna <luciano11v11@gmail.com>
2024-05-18 20:36:21 -0300

    fix app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CoursesController } from './courses/courses.controller';
-import { CoursesService } from './courses/courses.service';
-import { CoursesModule } from './courses/courses.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { CoursesModule } from './courses/courses.module'
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [CoursesModule, DatabaseModule],
-  controllers: [AppController, CoursesController],
-  providers: [AppService, CoursesService],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule { }
