8e6af5517153d1bf5f52a28486c4a35010b8fd8f
Luciano Vianna <luciano11v11@gmail.com>
2024-05-25 15:59:56 -0300

    criando tabela courses_tags

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -2,12 +2,16 @@ import { DataSource } from 'typeorm'
 import { dataSourceOptions } from './database.module'
 import { CreateCoursesTable1694705312546 } from 'src/migrations/1716661926777-CreateCoursesTable'
 import { CreateTagsTable1694706531026 } from 'src/migrations/1716662506251-CreateTagsTable'
+import { CreateCoursesTagsTable1695042750118 } from 'src/migrations/1716663143340-CreateCoursesTagsTable'
+import { AddCoursesIdToCoursesTagsTable1695043757120 } from 'src/migrations/1716663360319-AddCoursesIdToCoursesTagsTable'
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
   migrations: [
     CreateCoursesTable1694705312546,
-    CreateTagsTable1694706531026
+    CreateTagsTable1694706531026,
+    CreateCoursesTagsTable1695042750118,
+    AddCoursesIdToCoursesTagsTable1695043757120
   ],
 })
