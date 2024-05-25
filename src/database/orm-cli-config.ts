5a6a5deeac8d854a9a09991482c536ff0e0a111b
Luciano Vianna <luciano11v11@gmail.com>
2024-05-25 16:13:41 -0300

    add tags id na tabela courses_tags_tags

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -4,6 +4,7 @@ import { CreateCoursesTable1694705312546 } from 'src/migrations/1716661926777-Cr
 import { CreateTagsTable1694706531026 } from 'src/migrations/1716662506251-CreateTagsTable'
 import { CreateCoursesTagsTable1695042750118 } from 'src/migrations/1716663143340-CreateCoursesTagsTable'
 import { AddCoursesIdToCoursesTagsTable1695043757120 } from 'src/migrations/1716663360319-AddCoursesIdToCoursesTagsTable'
+import { AddTagsIdToCoursesTagsTable1695044746630 } from 'src/migrations/1716663676888-AddTagsIdToCoursesTagsTable'
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
@@ -12,6 +13,7 @@ export const dataSource = new DataSource({
     CreateCoursesTable1694705312546,
     CreateTagsTable1694706531026,
     CreateCoursesTagsTable1695042750118,
-    AddCoursesIdToCoursesTagsTable1695043757120
+    AddCoursesIdToCoursesTagsTable1695043757120,
+    AddTagsIdToCoursesTagsTable1695044746630
   ],
 })
