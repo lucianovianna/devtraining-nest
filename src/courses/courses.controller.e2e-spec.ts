f5b7176bd587ce379777ff9069a143bff02758f5
Luciano Vianna <luciano11v11@gmail.com>
2024-05-31 18:38:20 -0300

    cria testes e2e para o courses controller

--- src/courses/courses.controller.e2e-spec.ts
+++ src/courses/courses.controller.e2e-spec.ts
@@ -1,12 +1,12 @@
 import 'dotenv/config'
 import { Test, TestingModule } from '@nestjs/testing'
-import { CoursesController } from './courses.controller'
 import { INestApplication } from '@nestjs/common'
 import { Course } from './entities/courses.entity'
 import { DataSource, DataSourceOptions } from 'typeorm'
 import { Tag } from './entities/tags.entity'
 import { CoursesModule } from './courses.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import request from 'supertest'
 
 describe('CoursesController e2e tests', () => {
   let app: INestApplication
@@ -53,7 +53,82 @@ describe('CoursesController e2e tests', () => {
     await dataSource.destroy()
   })
 
-  // it('should be defined', () => {
-  //   expect(controller).toBeDefined()
-  // })
+  afterAll(async () => {
+    await module.close()
+  })
+
+  describe('POST /courses', () => {
+    it('should create a course', async () => {
+      const res = await request(app.getHttpServer())
+        .post('/courses')
+        .send(data)
+        .expect(201)
+      expect(res.body.id).toBeDefined()
+      expect(res.body.name).toEqual(data.name)
+      expect(res.body.description).toEqual(data.description)
+      expect(res.body.created_at).toBeDefined()
+      expect(res.body.tags[0].name).toEqual(data.tags[0])
+      expect(res.body.tags[1].name).toEqual(data.tags[1])
+    })
+  })
+
+  describe('GET /courses', () => {
+    it('should list all courses', async () => {
+      const res = await request(app.getHttpServer()).get('/courses').expect(200)
+      expect(res.body[0].id).toBeDefined()
+      expect(res.body[0].name).toEqual(data.name)
+      expect(res.body[0].description).toEqual(data.description)
+      expect(res.body[0].created_at).toBeDefined()
+      res.body.map(item =>
+        expect(item).toEqual({
+          id: item.id,
+          name: item.name,
+          description: item.description,
+          created_at: item.created_at,
+          tags: [...item.tags],
+        }),
+      )
+    })
+  })
+
+  describe('GET /courses/:id', () => {
+    it('should gets a course by id', async () => {
+      const res = await request(app.getHttpServer())
+        .get(`/courses/${courses[0].id}`)
+        .expect(200)
+      expect(res.body.id).toEqual(courses[0].id)
+      expect(res.body.name).toEqual(courses[0].name)
+      expect(res.body.description).toEqual(courses[0].description)
+    })
+  })
+
+  describe('PUT /courses/:id', () => {
+    it('should update a course', async () => {
+      const updateData = {
+        name: 'new name',
+        description: 'new description',
+        tags: ['one', 'two'],
+      }
+
+      const res = await request(app.getHttpServer())
+        .put(`/courses/${courses[0].id}`)
+        .send(updateData)
+        .expect(200)
+      expect(res.body.id).toEqual(courses[0].id)
+      expect(res.body.name).toEqual('new name')
+      expect(res.body.description).toEqual('new description')
+      expect(res.body.tags).toHaveLength(2)
+      expect(res.body.tags[0].name).toEqual('one')
+      expect(res.body.tags[1].name).toEqual('two')
+    })
+  })
+
+  describe('DELETE /courses/:id', () => {
+    it('should delete a course', async () => {
+      const res = await request(app.getHttpServer())
+        .delete(`/courses/${courses[0].id}`)
+        .expect(204)
+        .expect({})
+    })
+  })
 })
